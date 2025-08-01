437d663b38ce3d734b7a5e6a168b6f4f54ef85b7
Douglas Winter <douglas.winter@diamond.ac.uk>
2025-08-01 15:18:01 +0100

    Replace hardcoded backend URL with relative /api/ proxy path (#5)

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -4,17 +4,17 @@ import plansResponse from "./plans-response.json";
 const fakeTaskId = "7304e8e0-81c6-4978-9a9d-9046ab79ce3c";
 
 export const handlers = [
-  http.get("https://b01-1-blueapi.diamond.ac.uk/plans", () => {
+  http.get("/api/plans", () => {
     return HttpResponse.json(plansResponse);
   }),
 
-  http.put("https://b01-1-blueapi.diamond.ac.uk/worker/task", () => {
+  http.put("/api/worker/task", () => {
     return HttpResponse.json({
       task_id: fakeTaskId,
     });
   }),
 
-  http.post("https://b01-1-blueapi.diamond.ac.uk/tasks", () => {
+  http.post("/api/tasks", () => {
     return HttpResponse.json({
       task_id: fakeTaskId,
     });
