35c056eb4167b8c56fbd3e10a56d96c0863247e3
Douglas Winter <douglas.winter@diamond.ac.uk>
2025-08-13 16:05:39 +0100

    Redesign the JsonForms section

--- src/routes/Plans.tsx
+++ src/routes/Plans.tsx
@@ -3,26 +3,34 @@ import { Box } from "@mui/material";
 import { Link } from "react-router-dom";
 import { Breadcrumbs } from "@diamondlightsource/sci-react-ui";
 import { getPlans, type PlansResponse } from "../utils/api";
-import JsonPlanSelector from "../components/JsonPlanSelector";
 import VisrNavbar from "../components/VisrNavbar";
+import PlanBrowser from "../components/PlanBrowser";
+import PlanParameters from "../components/PlanParameters";
 
 function JsonFormsPlans() {
   const [planData, setPlanData] = useState<PlansResponse>({ plans: [] });
 
   useEffect(() => {
-    (async () => {
+    async function fetchPlans() {
       const results = await getPlans();
       setPlanData(results);
-    })();
+    }
+
+    fetchPlans();
   }, []);
 
   return (
     <>
       <VisrNavbar />
       <Breadcrumbs path={window.location.pathname} linkComponent={Link} />
-      <Box display={"flex"} justifyContent={"center"} sx={{ mt: 3, mb: 3 }}>
-        <JsonPlanSelector planResponse={planData} />
-      </Box>
+      <main>
+        <Box display={"flex"} justifyContent={"center"} sx={{ mt: 3, mb: 3 }}>
+          <PlanBrowser
+            plans={planData.plans}
+            renderPlan={plan => <PlanParameters plan={plan} />}
+          />
+        </Box>
+      </main>
     </>
   );
 }
