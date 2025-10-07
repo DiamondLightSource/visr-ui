a2becdeb7eeaefe48cdab319fc32eaceda56050e
Emily Arnold <emily.arnold@diamond.ac.uk>
2025-10-07 08:34:59 +0100

    Update visualisation on Spectroscopy page with 'R', 'G', 'B' fake images

--- src/components/RawSpectroscopyData.tsx
+++ src/components/RawSpectroscopyData.tsx
@@ -2,29 +2,75 @@ import { ImagePlot, type NDT } from "@diamondlightsource/davidia";
 import Box from "@mui/material/Box";
 import ndarray from "ndarray";
 
-function fakeImage(width: number, height: number): NDT {
-  const rgb = new Uint8Array(
+function fakeImages(width: number, height: number): [NDT, NDT, NDT] {
+  const r = new Uint8Array(
     Array.from({ length: width * height }, (_, i) => [
       (i % 4) * 255,
+      0,
+      0,
+    ]).flat(),
+  );
+  const g = new Uint8Array(
+    Array.from({ length: width * height }, (_, i) => [
+      0,
       (i % 3) * 255,
+      0,
+    ]).flat(),
+  );
+  const b = new Uint8Array(
+    Array.from({ length: width * height }, (_, i) => [
+      0,
+      0,
       (i % 2) * 255,
     ]).flat(),
   );
-  return ndarray(rgb, [width, height, 3]) as NDT;
+  return [
+    ndarray(r, [width, height, 3]) as NDT,
+    ndarray(g, [width, height, 3]) as NDT,
+    ndarray(b, [width, height, 3]) as NDT,
+  ];
 }
 
 function RawSpectroscopyData() {
-  const image = fakeImage(40, 25);
+  const [image_r, image_g, image_b] = fakeImages(40, 25);
   return (
-    <Box>
+    <Box
+      sx={{
+        display: "grid",
+        gridTemplateColumns: {
+          sm: "1fr",
+          md: "1fr 1fr 1fr",
+        },
+        gap: 3,
+        flexGrow: 1,
+      }}
+    >
       <ImagePlot
         aspect="auto"
         plotConfig={{
-          title: "Detector Data View",
+          title: "Detector Data View - R",
           xLabel: "x",
           yLabel: "y",
         }}
-        values={image}
+        values={image_r}
+      />
+      <ImagePlot
+        aspect="auto"
+        plotConfig={{
+          title: "Detector Data View - G",
+          xLabel: "x",
+          yLabel: "y",
+        }}
+        values={image_g}
+      />
+      <ImagePlot
+        aspect="auto"
+        plotConfig={{
+          title: "Detector Data View - B",
+          xLabel: "x",
+          yLabel: "y",
+        }}
+        values={image_b}
       />
     </Box>
   );
