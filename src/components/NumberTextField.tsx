0dfb3090c41b2e157b92ed4eeb836f3ab9475557
Emily Arnold <emily.arnold@diamond.ac.uk>
2025-12-04 17:28:16 +0000

    Update NumberTextField to NumberField instead of TextField

--- src/components/NumberTextField.tsx
+++ src/components/NumberTextField.tsx
@@ -1,48 +1,111 @@
-import { TextField } from "@mui/material";
+import * as React from "react";
+import { NumberField } from "@base-ui-components/react/number-field";
+import IconButton from "@mui/material/IconButton";
+import FormControl from "@mui/material/FormControl";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputAdornment from "@mui/material/InputAdornment";
+import InputLabel from "@mui/material/InputLabel";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
 import type { SpectroscopyFormData } from "./SpectroscopyForm";
 
-type NumberTextFieldProps = {
+export default function NumberTextField({
+  id: idProp,
+  formData,
+  setFormData,
+  field,
+  step,
+  label = field,
+  size = "medium",
+  ...other
+}: NumberField.Root.Props & {
   formData: SpectroscopyFormData;
   setFormData: (f: SpectroscopyFormData) => void;
   field: keyof SpectroscopyFormData;
   step: number;
-  label: string;
-};
+  label?: React.ReactNode;
+  size?: "small" | "medium";
+}) {
+  let id = React.useId();
+  if (idProp) {
+    id = idProp;
+  }
 
-const NumberTextField = ({
-  formData,
-  setFormData,
-  field,
-  step = 1,
-  label = field,
-}: NumberTextFieldProps) => {
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  const handleCommit = (
+    e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement, Element>,
   ) => {
-    const value = e.target.value;
-    const parsedValue =
-      value === ""
-        ? 0
-        : Number.isInteger(step) // parse to Int or Float depending on step
-          ? parseInt(value, 10)
-          : parseFloat(value);
-
-    setFormData({
-      ...formData,
-      [field]: parsedValue,
-    });
+    const parsedValue = Number.isInteger(step)
+      ? parseInt(e.target.value, 10)
+      : parseFloat(e.target.value);
+    setFormData({ ...formData, [field]: parsedValue });
   };
 
   return (
-    <TextField
-      fullWidth
-      label={label}
-      type="number"
+    <NumberField.Root
       value={formData[field]}
-      onChange={handleChange}
-      slotProps={{ htmlInput: { step: step } }}
-    />
+      step={step}
+      {...other}
+      render={(props, state) => (
+        <FormControl
+          size={size}
+          ref={props.ref}
+          disabled={state.disabled}
+          required={state.required}
+          variant="outlined"
+        >
+          {props.children}
+        </FormControl>
+      )}
+    >
+      <InputLabel htmlFor={id}>{label}</InputLabel>
+      <NumberField.Group>
+        <NumberField.Input
+          id={id}
+          render={(props, state) => (
+            <OutlinedInput
+              label={label}
+              inputRef={props.ref}
+              value={state.value}
+              onChange={props.onChange}
+              onKeyUp={props.onKeyUp}
+              onKeyDown={props.onKeyDown}
+              onBlur={e => handleCommit(e)}
+              onFocus={props.onFocus}
+              slotProps={{ input: props }}
+              fullWidth={true}
+              endAdornment={
+                <InputAdornment
+                  position="end"
+                  sx={{
+                    flexDirection: "column",
+                    maxHeight: "unset",
+                    alignSelf: "stretch",
+                    borderLeft: "1px solid",
+                    borderColor: "divider",
+                    ml: 0,
+                    "& button": { py: 0, flex: 1, borderRadius: 0.5 },
+                  }}
+                >
+                  <NumberField.Increment render={<IconButton size={size} />}>
+                    <KeyboardArrowUpIcon
+                      fontSize={size}
+                      sx={{ transform: "translateY(2px)" }}
+                    />
+                  </NumberField.Increment>
+                  <NumberField.Decrement render={<IconButton size={size} />}>
+                    <KeyboardArrowDownIcon
+                      fontSize={size}
+                      sx={{ transform: "translateY(-2px)" }}
+                    />
+                  </NumberField.Decrement>
+                </InputAdornment>
+              }
+              sx={{ pr: 0 }}
+            />
+          )}
+        />
+      </NumberField.Group>
+    </NumberField.Root>
   );
-};
-
-export default NumberTextField;
+}
