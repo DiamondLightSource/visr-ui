3adb4d4255679b84b65fdf9e90cbe8f6b70eca09
Douglas Winter <douglas.winter@diamond.ac.uk>
2025-12-12 08:27:59 +0000

    Automatically trigger visr-reconstruction workflow at scan end

--- src/components/spectroscopy/SpectroscopyView.tsx
+++ src/components/spectroscopy/SpectroscopyView.tsx
@@ -1,6 +1,11 @@
 import { Box } from "@mui/material";
 import RawSpectroscopyData from "./RawSpectroscopyData";
 import { SpectroscopyForm } from "./SpectroscopyForm";
+import { useEffect } from "react";
+import { useScanEvents } from "../../hooks/scanEvents";
+import { useSubmitWorkflow } from "../../hooks/useSubmitWorkflow";
+import { useInstrumentSession } from "../../context/instrumentSession/useInstrumentSession";
+import { visitTextToVisit } from "../../utils/common";
 
 export type SpectroscopyFormData = {
   total_number_of_scan_points: number;
@@ -11,6 +16,26 @@ export type SpectroscopyFormData = {
 };
 
 function SpectroscopyView() {
+  // set off workflow when scan ends
+  const scanEvent = useScanEvents();
+  const { instrumentSession } = useInstrumentSession();
+  useEffect(() => {
+    async function submitWorkflow() {
+      const submit = useSubmitWorkflow("visr-reconstruction");
+      await submit(visitTextToVisit(instrumentSession)!, {
+        inpath: scanEvent?.filepath,
+      });
+    }
+    if (!scanEvent || !instrumentSession) return;
+    if (scanEvent.status == "finished") {
+      try {
+        submitWorkflow();
+      } catch (err) {
+        console.error("Error triggering workflow", err);
+      }
+    }
+  });
+
   return (
     <Box
       sx={{
